0db170c8a0a22dab7d424a4452a3a0054d37925a
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-06-19 15:49:23 +0700

    Submit #1
    Challenge 9:
    
    Contact apps dengan React dan custom css.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="Card">
+      <div className="CardHeader">
+        <img className="Avatar" src={data.photo} alt={data.photo} />
+      </div>
+      <div className="CardBody">
+        <h2>{data.name}</h2>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div className="Header">
+      <div className="HeaderTitle"> Call a friend</div>
+      <div className="HeaderSubtitle"> Your friendly contact app</div>
+    </div>
+  );
+};
 
 export default Header;
